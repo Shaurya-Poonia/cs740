3a3935effd563cf873041f8c506d9ea69bc16d80
Shaurya-Poonia <shauryapoonia@gmail.com>
2015-09-22 03:10:24 +0530

    code almost finished

---
+++ task3/readme.txt
@@ -0,0 +1,2 @@
+Write group members (name, roll number), group number, honour code, citations
+etc. in this file. Also, write individual percentages such as Student 1: 80%, Student 2:100% in this file. 
---
+++ task3/tree.m
@@ -0,0 +1,58 @@
+%% Program which finds page ranks of pages in "sink" graph %%%
+
+inputFile = "in.txt";
+%arg_list = argv ();
+%inputFile = arg_list{1};
+
+%c = str2double(arg_list{2});
+data = dlmread (inputFile);
+c  = .15;
+% your code goes here
+
+
+%% read DATA from file
+data = dlmread (inputFile);
+n = size(data,1);
+A = zeros(n);
+%construct Adjancy matrix
+[row,col] = size(data);
+for i = 1:row
+  for j = 1:col
+    x =data(i,j); 
+    if x!=0
+      A(i,x)=1;
+    end
+  end
+end
+
+%remove self loops
+A = A - diag(diag(A));
+
+%% construct C matri
+
+%% normalize every row
+
+
+C = zeros(row,row);
+for j = 1:n
+    outdegree = sum(A(j,:)>0);  
+    if outdegree >0
+      for i = 1:n
+        if(A(j,i)>0 )
+            C(i,j) = 1/outdegree;
+        end   
+      end
+    elseif outdegree==0
+      C(:,j) = 1/n;
+    end
+end
+M = C + (c/n)*ones(n);
+[eigvec,eigval] = eig(M);
+d = 1.0+c;
+eigval = real(eigval);
+[x,y] = find(abs(eigval-d)<=1e-6);
+Rank_vector = eigvec(:,y);
+if sum(Rank_vector<0)==n
+  Rank_vector = -Rank_vector;
+ end
+Rank_vector
