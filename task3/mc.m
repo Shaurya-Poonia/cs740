4d4fc407b044fdde98e95bf14f8e49782d8c44ea
Shaurya-Poonia <shauryapoonia@gmail.com>
2015-09-22 05:46:43 +0530

    final

--- task3/mc.m
+++ task3/mc.m
@@ -1,12 +1,12 @@
 %% Program which finds page ranks of pages in "sink" graph %%%
 
-inputFile = "in.txt";
-%arg_list = argv ();
-%inputFile = arg_list{1};
+%inputFile = "in.txt";
+arg_list = argv ();
+inputFile = arg_list{1};
 
-%c = str2double(arg_list{2});
+c = str2double(arg_list{2});
 data = dlmread (inputFile);
-c  = .15;
+%c  = 0.15;
 % your code goes here
 
 
@@ -14,7 +14,8 @@ c  = .15;
 data = dlmread (inputFile);
 n = size(data,1);
 A = zeros(n);
-%construct Adjancy matrix
+
+%construct Adjancy matrix with coloumn sum =1
 [row,col] = size(data);
 for i = 1:row
   for j = 1:col
@@ -28,9 +29,8 @@ end
 %remove self loops
 A = A - diag(diag(A));
 
-%% construct C matri
 
-%% normalize every row
+%% construct C matrix %% normalize every row
 
 
 C = zeros(row,row);
@@ -46,9 +46,53 @@ for j = 1:n
       C(:,j) = 1/n;
     end
 end
+
+% Construct M matrix  
 M = C + (c/n)*ones(n);
+
+% calculate eigenvalues and eigenvector
 [eigvec,eigval] = eig(M);
+
+%eigenvector correspond to eigenvalues(d) look for
 d = 1.0+c;
+
+
+% find the eigvector correspond to d
 eigval = real(eigval);
 [x,y] = find(abs(eigval-d)<=1e-6);
-Rank_vector = eigvec(:,y)
+value_vector = eigvec(:,y);
+x = size(value_vector,2);
+for ev = 1: x 
+  %this vector should be positive
+   if sum(value_vector(:,ev)<0)==n
+    value_vector(:,ev) = -value_vector(:,ev);
+   end
+  disp(' possible rankings:');
+  % sorting the value 
+  [value,RANK] = sort(value_vector(:,ev),"descend");
+
+  RankofPage = zeros(n,1); % contains  rank of every page
+
+  % calculate the actual Rank 
+  counter =1;
+  z = -1;
+  add =0;
+  for i = 1:n
+     if(abs(value(i)-z)>=1e-12)
+        counter = counter +add;
+        z = value(i);
+        RankofPage(RANK(i)) = counter;
+        add=1;
+     else
+        RankofPage(RANK(i)) = counter;
+        add = add+1;
+     end
+  end
+
+  %% print only top 20 pages
+  disp("Top 20(n) pages: ");
+  disp("    ID  RANK ");  
+  TOP = [ RANK(1:n), RankofPage(RANK(1:n))];
+  TOP20= TOP(1:min(20,n),:);
+  disp(TOP20);
+end
--- task3/readme.txt
+++ task3/readme.txt
@@ -1,2 +1,17 @@
-Write group members (name, roll number), group number, honour code, citations
+Write group members (name, roll number),
 etc. in this file. Also, write individual percentages such as Student 1: 80%, Student 2:100% in this file. 
+
+ group 01
+ name
+ saurabh jain,   143050048	 (100%)
+ vivek,          143059005   (100%)
+ 
+------------------------------------------
+Honor Code :- The code we have written is completely implemented by us. We have taken help of references during implementation.
+
+refrence:
+http://www.math.cornell.edu/~mec/Winter2009/RalucaRemus/Lecture3/lecture3.html
+https://en.wikipedia.org/wiki/PageRank
+
+
+
